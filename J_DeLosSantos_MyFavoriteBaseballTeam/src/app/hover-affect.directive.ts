61be296ae9a8df11ba197b11cb864d120755cb2b
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-02-22 11:27:59 -0500

    Added logic for changing border when hover content card

--- J_DeLosSantos_MyFavoriteBaseballTeam/src/app/hover-affect.directive.ts
+++ J_DeLosSantos_MyFavoriteBaseballTeam/src/app/hover-affect.directive.ts
@@ -1,22 +1,36 @@
-import { Directive, HostBinding, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]',
   standalone: true
 })
 export class HoverAffectDirective {
-  @Input() affectStyle: 'underline' | 'bold' ='underline';
+  @Input() affectStyle: 'underline' | 'bold' | 'border' ='underline';
   @HostBinding('style.textDecoration') textDecoration: string = "";
   @HostBinding('style.fontWeight') fontWeight: string = "";
-  constructor() { }
+  @HostBinding('style.border') border: string = "'";
+  private originalBorder: string = "";
+  
+  constructor(private el: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
     if (this.affectStyle === 'underline') this.textDecoration = 'underline'; 
+    else if (this.affectStyle === 'border') {
+      this.originalBorder = window.getComputedStyle(this.el.nativeElement).border;
+      this.setBorder('2px solid red'); 
+    }
     else this.fontWeight = 'bold';
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     if (this.affectStyle === 'underline') this.textDecoration = 'none';
+    else if(this.affectStyle === 'border'){
+      this.setBorder(this.originalBorder);
+    }
     else this.fontWeight = 'normal';
+  }
+
+  private setBorder(value: string) {
+    this.border = value;
   }
 }
