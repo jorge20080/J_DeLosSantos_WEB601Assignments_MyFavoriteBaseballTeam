5f240ec935b1b13b9e67fad41e2d5d983b72be78
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-03-06 11:30:04 -0500

    Added bonus part to validate fields

--- J_DeLosSantos_MyFavoriteBaseballTeam/src/app/create-content/create-content.component.ts
+++ J_DeLosSantos_MyFavoriteBaseballTeam/src/app/create-content/create-content.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, NgModule, Output } from '@angular/core';
 import { Event } from '@angular/router';
 import { Content } from '../helper-files/content-interface';
@@ -5,23 +6,25 @@ import { Content } from '../helper-files/content-interface';
 @Component({
   selector: 'app-create-content',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './create-content.component.html',
   styleUrl: './create-content.component.scss'
 })
 export class CreateContentComponent {
   @Output() createContent = new EventEmitter<Promise<Content>>();
-  
+  invalidRequest: boolean|null = false;
   onSubmit(e:SubmitEvent, content: Content ) {
 
     const sendContent = new Promise<any>((resolve,reject)=>{
       setTimeout(() => {
-        if(content.title === ""){
+        if(content.title ==="" || content.creator==="" || content.description==="" || isNaN(content.id)){
+          this.invalidRequest = true;
           reject();
         }
         else {
           console.log('Content added successfully:', content.title);
           resolve(content);
+          this.invalidRequest = false;
         }
       }, 1000);
     })
